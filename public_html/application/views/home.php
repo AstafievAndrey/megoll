0e9124b3e700de96c5c21437365aadf11c406740
andrejastafev <carrats@yandex.ru>
2015-10-21 09:56:28 +0300

    первая инициализация

---
+++ public_html/application/views/home.php
@@ -0,0 +1,318 @@
+<html >
+    <head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="<?php echo HTTP_CSS_PATH; ?>favicon.png"> 
+    <title><?php echo $title;?></title>
+    <meta name="description" content="<?php echo $des;?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="<?php echo HTTP_CSS_PATH; ?>style.css" rel="stylesheet"> 
+        <link href="<?php echo HTTP_CSS_PATH; ?>style.less" rel="stylesheet"> 
+       <link  href="<?php echo HTTP_CSS_PATH; ?>bootstrap.min.css" rel="stylesheet" > 
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+        <script src="<?php echo HTTP_JS_PATH; ?>/bootstrap.min.js"></script> 
+   
+  </head>
+  <body>
+    <div class="cover">
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="cola navbar-brand" href="#"><span>Megoll.com</span></a>
+          </div>
+          <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+            
+            <ul class="cola nav navbar-nav navbar-right">
+               
+                <li >
+                <a class="cola" href="#aboutus">О Нас</a>
+              </li>
+              <li>
+                <a class="cola" href="#contacts" >Контакты</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="cover-image" style="background-image: url(http://www.motto.net.ua/old_site//img/arhitektura/1283963378_D7E5F0F2E5E6203033203033.jpg);"></div>
+      <div class="container">
+          
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h1 class="text-inverse">Megoll.com</h1>
+            <p class="text-inverse">Разработка и продвижение мобильных приложений и сайтов.</p>
+            <br>
+            <br>
+            <a class="btn btn-lg btn-primary">Нажми</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+           <div class="row text-center">
+          <div class="col-md-12">
+            <h3>БУДЬТЕ С НАМИ</h3>
+            <br>
+            <br>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-xs-3 text-center">
+              <a name="contacts" href="http://instagram.com/fanis_nl" target="_blank" ><i class="fa fa-5x fa-fw fa-instagram"></i></a> 
+          </div>
+          <div class="col-xs-3">
+              <a href="http://twitter.com/megoll.com" target="_blank" ><i class="fa fa-5x fa-fw fa-twitter"></i></a>
+          </div>
+          <div class="col-xs-3">
+            <a href="https://facebook.com/megoll.com" target="_blank" ><i class="fa fa-5x fa-fw fa-facebook"></i></a>
+          </div>
+          <div class="col-xs-3 text-center">
+              <a href="https://vk.com/megoll" target="_blank"><i class="fa fa-5x fa-fw fa-vk"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-12">
+            <h3>Специальные цены</h3>
+            <br>
+            <br>
+          </div>
+        </div>
+        <div class="row db-padding-btm db-attached">
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <div class="db-wrapper">
+              <div class="db-pricing-eleven db-bk-color-one">
+                <div class="price">
+                  <sup>₽</sup>5000
+                  <small>от</small>
+                </div>
+                <div class="type">Landing page</div>
+                <ul>
+                  <li>
+                    <i class="fa fa-pencil"></i>Уникальный дизайн</li>
+                  <li>
+                    <i class="fa fa-sort-alpha-asc"></i>Подбор домена</li>
+                  <li>
+                    <i class="fa fa-desktop"></i>Адаптивность</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <div class="db-wrapper">
+              <div class="db-pricing-eleven db-bk-color-two popular">
+                <div class="price">
+                  <sup>₽</sup>40000
+                  <small>от</small>
+                </div>
+                <div class="type">Приложения</div>
+                <ul>
+                  <li>
+                    <i class="fa fa-mobile"></i>Android</li>
+                  <li>
+                    <i class="fa fa-user"></i>Регистрация в Google Play</li>
+                  <li>
+                    <i class="fa fa-kayboard-o"></i>Продвижение</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <div class="db-wrapper">
+              <div class="db-pricing-eleven db-bk-color-three">
+                <div class="price">
+                  <sup>₽</sup>3000
+                  <small>от</small>
+                </div>
+                <div class="type">Реклама</div>
+                <ul>
+                  <li>
+                    <i class="fa fa-search"></i>Подбор ключевых слов</li>
+                  <li>
+                    <i class="fa fa-file-text-o"></i>20 объявлений</li>
+                  <li>
+                    <i class="fa fa-icon-ya"></i>Яндекс</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <div class="db-wrapper">
+              <div class="db-pricing-eleven db-bk-color-six">
+                <div class="price">
+                  <sup>₽</sup>10000
+                  <small>от</small>
+                </div>
+                <div class="type">СЕО</div>
+                <ul>
+                  <li>
+                    <i class="fa fa-search"></i>Подбор ключевых слов</li>
+                  <li>
+                    <i class="fa fa-file-text-o"></i>Уникальный текст</li>
+                  <li>
+                    <i class="fa fa-sort-numeric-asc"></i>Вывод в топ</li>
+                </ul>
+              </div>
+                
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="text-primary">Адаптивный веб-дизайн</h1>
+            <h3>Что же это такое? </h3>
+            <p>Адаптивный веб-дизайн — дизайн веб-страниц,
+                обеспечивающий корректное отображение сайта на различных устройствах, 
+                подключённых к интернету и динамически подстраивающийся под заданные размеры окна браузера.
+
+Целью адаптивного веб-дизайна является универсальность веб-сайта для различных устройств. 
+Для того, чтобы веб-сайт был удобно просматриваемым с устройств различных разрешений и форматов,
+по технологии адаптивного веб-дизайна не нужно создавать отдельные версии веб-сайта для отдельных видов
+устройств. Один сайт может работать на смартфоне, планшете, ноутбуке и телевизоре с выходом в интернет, 
+то есть на всем спектре устройств.</p>
+          </div>
+          <div class="col-md-6">
+                <img src="<?php echo base_url('image/Responsive_Design.jpg'); ?>" 
+            class="img-responsive"/>
+         
+          
+          </div>
+        </div>
+      </div>
+    </div>
+      
+     <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="text-primary">Поиско́вая оптимиза́ция</h1>
+            <h3>Что за зверь?</h3>
+            <p>
+               Поиско́вая оптимиза́ция (англ. search engine optimization, SEO) — комплекс мер по внутренней и внешней оптимизации, для поднятия позиций сайта в результатах выдачи поисковых систем по определенным запросам пользователей, с целью увеличения трафика (для инфоресурсов) и потенциальных клиентов (для коммерческих ресурсов) и последующей монетизации этого трафика.
+
+Обычно, чем выше позиция сайта в результатах поиска, тем больше заинтересованных посетителей переходит на него с поисковых систем. При анализе эффективности поисковой оптимизации оценивается стоимость целевого посетителя с учётом времени вывода сайта на указанные позиции и конверсии сайта. .</p>
+          </div>
+          <div class="col-md-6"> 
+           <img src="<?php echo base_url('image/seo_slider6_inner.png'); ?>" 
+            class="img-responsive"/>
+          </div>
+        </div>
+      </div>
+     </div >
+     
+          <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="text-primary">Мобильные приложения</h1>
+            <h3>Для кого оно?</h3>
+            <p>
+                Различные инициативы существуют от мобильных операторов и от производителей. Разработчики приложений могут предлагать и публиковать свои программы в магазинах приложений, с возможностью зарабатывать от распределения доходов по продажам. Самым известными являются App Store Apple, где только одобренные приложения могут распространяться и запускаться на iOS устройствах (также известно как walled garden), и Android Market Google, приложения в котором работают на устройствах с Android OS. HP / Palm также имеют Palm App Catalog, где пользователи устройств на HP / Palm webOS могут загружать приложения непосредственно с устройства или отправить ссылку на приложение с помощью уникального метода распространения. Мобильные операторы Telefonica Group и Telecom Italia запустили межплатформенный магазин приложений для своих абонентов. Производитель мобильных устройств Nokia запустил Ovi app store для смартфонов Nokia.
+                
+       </p>
+          </div>
+          <div class="col-md-6"> 
+           <img src="<?php echo base_url('image/iPhone_GUI.jpg'); ?>" 
+            class="img-responsive"/>
+          </div>
+        </div>
+      </div>
+     </div >
+     
+     
+          <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="text-primary">Контекстная реклама</h1>
+            <h3>Как работает ?</h3>
+            <p>
+              тематикой рекламируемого товара либо услуги, целевой аудитории, что повышает вероятность их отклика на рекламу.
+
+Для определения соответствия рекламного материала странице интернет-сайта обычно используется принцип ключевых слов. На ключевые слова ориентируются и поисковые системы. Поэтому контекстная реклама с большей вероятностью будет продемонстрирована потребителю, который использует сеть Интернет для поиска интересующей информации о товарах или услугах.
+
+Почти все поисковые машины в сети Интернет используют системы контекстной рекламы для получения прибыли (например, рекламная сеть Google AdWords является источником прибыли компании Google). Системы контекстной рекламы позволяют размещать рекламу как на страницах с результатами поиска по определенным ключевым словам, так и на сайтах, установивших блоки контекстной рекламы на своих страницах.  
+       </p>
+          </div>
+          <div class="col-md-6"> 
+           <img src="<?php echo base_url('image/ad.svg'); ?>" 
+            class="img-responsive"/>
+           
+          </div>
+        </div>
+      </div>
+     </div >
+      
+      
+     <hr>
+     
+     <div class="row">
+         <div class="map">
+             <a href="#" name="map"></a>
+             
+         </div>
+     </div>
+      
+       
+      
+      
+    <footer class="section section-primary">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <h1>Цитата из жизни</h1>
+            <a href="#" name="aboutus"></a>
+            <p >Плох тот военачальник, у которого заготовлен только один путь к победе.
+            </p>
+          </div>
+          <div class="col-sm-6">
+              <p class="text-info text-right" style="color: #fff;">
+              Поделиться в соц сетях 
+              
+            </p>
+         
+            <div class="row">
+              <div class="col-md-12 hidden-xs text-right">
+                <a href="#"
+   onclick="this.href='http://instagram.com/oembed?url='+window.location.href+window.location.hash">
+   <i class="fa fa-3x fa-fw fa-instagram text-inverse"></i>
+                </a>
+               
+                <a href="#"
+   onclick="this.href='http://twitter.com/timeline/home?status='+document.title+'%20'+window.location.href+window.location.hash">
+   <i class="fa fa-3x fa-fw fa-twitter text-inverse"></i>
+               </a>
+                  
+               <a href="#"
+   onclick="this.href='http://www.facebook.com/share.php?u='+window.location.href+window.location.hash">
+   <i class="fa fa-3x fa-fw fa-facebook text-inverse"></i>
+               </a>
+                  
+                 <a href="#"
+   onclick="this.href='https://vk.com/share.php?url='+window.location.href+window.location.hash+'&title='+document.title">
+   <i class="fa fa-3x fa-fw fa-vk text-inverse"></i>
+                 </a>
+                  
+                 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  
+
+</body></html>
