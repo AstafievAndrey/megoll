50789ed781e5fb0e4ac54050ebb9badfa01bab84
andrejastafev <carrats@yandex.ru>
2015-10-30 09:50:02 +0300

    Доделала страницу с мобильными приложениями

--- public_html/application/views/pages/mobile_app.php
+++ public_html/application/views/pages/mobile_app.php
@@ -1,10 +1,136 @@
-<div class="container-fluid" style="margin-top: 20px;">
+<div class="container-fluid" style="margin-top: 70px;">
     <div class="container">
 	<div class="row">
-	    <div class="col-sm-12">
-		1
+	    <div class="col-sm-7">
+		<h1>Разработка мобильных приложений под <img src="<?php echo IMG;?>mobile_app/svg/android.svg" height="32"  alt="лого android" /> android и <img src="<?php echo IMG;?>mobile_app/svg/ios.svg" height="32"  alt="лого ios" /> ios.</h1>
+		<p>
+		    <strong>Мобильные устройства</strong> стали частью нашей жизни, если взглянуть на <strong>статистику</strong> использования мобильных устройств , то можно увидеть с какими темпами заводы печатают мобильные устройства и это всего лишь за несколько лет. Уже давно прошло то время когда мы удивлялись при виде первых беспроводных телефонов(кирпичей),  время идет нам и вам нужно привыкать новому. Можно уверено сказать, что  наступило эра  <strong>Всемирной Мобилизации</strong>.
+		</p>
+		<p>
+		    Как же мобильное устройство без приложения и игрушек! Сколько людей пользуются мобильным приложениям? Почти <strong>каждый второй</strong> и количество пользователей с каждым годом растет как на дрожжах и все это, несмотря на то, что, <i>казалось бы</i>, сложность этих программ.
+		</p>
+		<p>
+		    Мы те кто занимается  созданием мобильных приложений для среднего и крупного бизнеса, но сперва как создать мобильное приложение мы спросим у вас : <strong>Нужно ли мобильное приложение вашему бизнесу?</strong>
+		</p>
+	    </div>
+	    <div class="col-sm-5">
+		<img src="<?php echo IMG;?>mobile_app/android.jpg" class="img-responsive"  alt="забавный android" />
 	    </div>
 	</div>
+	<div class="row">
+	    <div class="col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/apple-vs-android.jpg" class="img-responsive"  alt="apple vs android" />
+	    </div>
+	    <div class="col-sm-9">
+		<p>
+		    Когда к нам обращаются с вопросом о  <i>разработке мобильного приложения</i>, мы тоже задаемся этим вопросом. Нужно ли оно вам? Или можно ограничиться созданием сайта с базовой оптимизацией и правильной настройкой контекстной рекламы. Ответить на этот вопрос сходу <strong>нельзя</strong>! Только после полного аудита вашего бизнеса и анализа конкурентов мы сможем ответить на этот вопрос и обосновать причины <i>не нужности или наоборот необходимости вам мобильного приложения</i>. Уже после этого можно поговорить о стоимости разработки и приступить к написанию технического задания.
+		</p>
+		<p>
+		    По опыту можно сказать, что российский рынок в этой сфере остается пока диким, и многие ниши еще пока не заняты, продлится такое не долго, максимум пару лет и рынок мобильных приложений станет насыщен. 
+		</p>
+	    </div>
+	    <div class="col-sm-12">
+		<p>
+		    <h2 style="font-size: 18px;">Если вы настроены серьезно по разработке приложения, дадим вам шесть советов:</h2>
+		</p>
+		<ul>
+		    <li>
+			<h3>Не бегите за модой, начните с малого. Постепенно, мониторя рынок вы можете добавлять нужные и интересные пользователям функции;</h3>
+		    </li>
+		    <li>
+			<h3>Четко представляйте результат к которому стремитесь, сядьте и напишите десять пунктов того, что вы хотите. А не некого мифического розового слоника, который будет таскать вам мешки с деньгами;</h3>
+		    </li>
+		    <li>
+			<h3>Готовьтесь к тому что траты на продвижение приложения будут превышать траты на его создание;</h3>
+		    </li>
+		    <li>
+			<h3>Не нарвитесь на шарлатанов, которые будут делиться с вами секретными алгоритмами по продвижению в Android Market и App Store, в первую очередь важно качество того, что вы предлагаете пользователю;</h3>
+		    </li>
+		    <li>
+			<h3>Используйте все возможные каналы продвижения(тематические форумы, выпуск новых обновлений, публикация новостей);</h3>
+		    </li>
+		    <li>
+			<h3>Имея малый бюджет(<100 тыс.), будьте готовы к долгому результату.</h3>
+		    </li>
+		</ul>
+	    </div>
+	</div>
+    </div>
+</div>
+<div class="container-fluid bck">
+    <div class="container">
+	<div class="row" style="text-align: center;">
+	    <h2>
+		Как мы работаем
+	    </h2>
+	</div>
+	<div class="row work">
+	    <div class="col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/settings.svg" height="64"  alt="лого android" />
+		<br><h4>Вырабатываем четкие и понятные требования для приложения</h4>
+	    </div>
+	    <div class="col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/chair.svg" height="64"  alt="лого android" />
+		<br><h4>Проект мобильного приложения прост и понятен для конечного пользователя</h4>
+	    </div>
+	    <div class="col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/smart.svg" height="64"  alt="лого android" />
+		<br><h4>Мы изучаем и стараемся быть знакомыми с вашей областью деятельности</h4>
+	    </div>
+	    <div class="col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/shield.svg" height="64"  alt="лого android" />
+		<br><h4>Разработка Приложения не связано с особым риском</h4>
+	    </div>
+	</div>
+	<div class="row work">
+	    <div class="col-sm-offset-3 col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/clock.svg" height="64"  alt="лого android" />
+		<br><h4>Четкое определение времени реализации и окупаемости приложения</h4>
+	    </div>
+	    <div class="col-sm-offset-0 col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/money.svg" height="64"  alt="лого android" />
+		<br><h4>Затраты на изменение требований к мобильному приложению могут оказаться высокими</h4>
+	    </div>
+	</div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container">
+	<div class="row" style="text-align: center;">
+	    <h2>
+		Как работают большинство
+	    </h2>
+	</div>
+	
+	<div class="row work">
+	    <div class="col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/unknown.svg" height="64"  alt="лого android" />
+		<br><h4>Непонятные требования и что мы в конечном итоге хотим получить</h4>
+	    </div>
+	    <div class="col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/bald.svg" height="64"  alt="лого android" />
+		<br><h4>Проект приложения сложен и непонятен и не ясно, что мы получим в конце</h4>
+	    </div>
+	    <div class="col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/man.svg" height="64"  alt="лого android" />
+		<br><h4>Не знание прикладной области</h4>
+	    </div>
+	    <div class="col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/danger.svg" height="64"  alt="лого android" />
+		<br><h4>Высокий риск развала проекта</h4>
+	    </div>
+	</div>
+	<div class="row work">
+	    <div class="col-sm-offset-3 col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/indifferent.svg" height="64"  alt="лого android" />
+		<br><h4>Предсказуемость и временные рамки не играют особой роли</h4>
+	    </div>
+	    <div class="col-sm-offset-0 col-sm-3">
+		<img src="<?php echo IMG;?>mobile_app/svg/coin.svg" height="64"  alt="лого android" />
+		<br><h4>Затраты на изменение требований к мобильному приложению окажутся низкими</h4>
+	    </div>
+	</div>
+
     </div>
 </div>
 
--- public_html/application/views/public/head.php
+++ public_html/application/views/public/head.php
@@ -6,7 +6,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 	<title><?php echo $title;?></title>
-	<meta name="description" content="<?php echo $desc;?>" />
 	<link rel="shortcut icon" href="<?php echo IMG;?>favicon.png" type="image/png">
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
